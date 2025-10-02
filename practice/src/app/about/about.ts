f2cde101b815ff7daa29d005ec5ab6aa0a4c78df
beereket <b_yergali@outlook.com>
2025-10-03 00:48:18 +0500

    data binding

--- practice/src/app/about/about.ts
+++ practice/src/app/about/about.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule], // <--- добавили сюда
   templateUrl: './about.html',
-  styleUrl: './about.css'
+  styleUrls: ['./about.css']
 })
 export class About {
+  id = '22B030344';
+  name = 'Bereket Yergali';
+  university = 'KBTU';
+  email = 'b_yergali@kbtu.kz';
+  practice = 2;
 
+  travellerImg = 'assets/images/about/traveller.png';
+  isButtonDisabled = false;
+  likes = 0;
+
+  showMessage = false;
+
+  inputName = '';
+  inputEmail = '';
+  subscribed = false;
+
+  addLike() {
+    this.likes++;
+  }
+
+  toggleMessage() {
+    this.showMessage = !this.showMessage;
+  }
+
+  subscribe() {
+    if (this.inputEmail.trim() !== '') {
+      this.subscribed = true;
+    }
+  }
 }
